d2049cffb3f52672bfada005db156977316bf3c5
Balin Warren <balin.warren@gmail.com>
2024-01-05 21:05:18 -0700

    Completed part 1 and part 2 of Day 4

--- day 4/scratchcards.py
+++ day 4/scratchcards.py
@@ -19,5 +19,56 @@ def split_lists(file):
 
     return cards
 
+def count_points(card):
+    points = 0
 
-print(split_lists(file)[10])
+    for num in card[1]:
+        if num.isdigit() and num in card[0]:
+            if points > 0:
+                points *= 2
+            else:
+                points = 1
+
+    return points
+
+def count_new_scratchers(cards):
+    wins = []
+    instances = []
+
+    for card in cards:
+        win_num = 0
+
+        for num in card[1]:
+            if num.isdigit() and num in card[0]:
+                win_num += 1
+
+        wins.append(win_num)
+        instances.append(1)
+
+    index = 0
+    for inst in instances:
+        for i in range(inst):   
+            for j in range(1, wins[index] + 1):
+                instances[index + j] += 1
+
+        index += 1
+
+    sum = 0
+    for inst in instances:
+        sum += inst
+
+    return sum
+    
+
+#part 1 logic
+cards = split_lists(file)
+sum = 0
+
+for card in cards:
+    points = count_points(card)
+    sum += points
+
+print(sum)
+
+#part 2 logic
+print(count_new_scratchers(cards))
